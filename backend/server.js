2c9b3b5490af8a7292a5e59c9a4b0b306dd628ee
dptole <danillo.paiva.toledo@gmail.com>
2020-01-24 02:52:24 -0300

    - Avoid responding with compressed cached response for unsupporting request.

--- backend/server.js
+++ backend/server.js
@@ -98,28 +98,30 @@ util._extend(server, {
       server.cache.map.has(route_url),
 
     store: (full_filepath, options) => {
-      let filestream = null
+      let filestream = fs.createReadStream(full_filepath)
+      let compressstream = null
+      let compression = ''
 
-      if(options && options.compression === 'brotli')
-        filestream = fs.createReadStream(full_filepath).pipe(
-          zlib.createBrotliCompress({
+      switch(options && options.compression) {
+        case 'brotli':
+          compression = options.compression
+          compressstream = zlib.createBrotliCompress({
             params: {
               [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
               [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
               [zlib.constants.BROTLI_PARAM_SIZE_HINT]: fs.statSync(full_filepath).size
             }
           })
-        )
+          break;
 
-      else if(options && options.compression === 'gzip')
-        filestream = fs.createReadStream(full_filepath).pipe(
-          zlib.createGzip({
+        case 'gzip':
+          compression = options.compression
+          compressstream = zlib.createGzip({
             level: zlib.constants.Z_BEST_COMPRESSION
           })
-        )
+      }
 
-      else
-        filestream = fs.createReadStream(full_filepath)
+      if(compressstream) filestream = filestream.pipe(compressstream)
 
       let b = Buffer.from('')
 
@@ -128,7 +130,7 @@ util._extend(server, {
 
       const onEnd = () => {
         filestream.removeListener('data', onData)
-        server.cache.map.set(full_filepath, {b, options})
+        server.cache.map.set(compression + ':' + full_filepath, {b, options})
       }
 
       filestream.on('data', onData)
@@ -136,7 +138,13 @@ util._extend(server, {
     },
 
     get: (route_url, req, res) => {
-      const {b, options} = server.cache.map.get(route_url)
+      let compression = req.supportsBrotli()
+        ? 'brotli'
+        : req.supportsGzip()
+        ? 'gzip'
+        : ''
+
+      const {b, options} = server.cache.map.get(compression + ':' + route_url)
       const pt = new stream.PassThrough
       pt.push(b)
       pt.push(null)
